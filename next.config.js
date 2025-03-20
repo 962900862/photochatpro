c4b33cf61417ff7fcdc0d3103e51379e4fe21302
962900862 <82793210+962900862@users.noreply.github.com>
2025-03-21 00:16:15 +0800

    更新配置：区分开发和生产环境，更新GitHub Actions部署配置

--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  basePath: process.env.NODE_ENV === 'production' ? '/photochatpro' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/photochatpro/' : '',
   trailingSlash: true,
+  distDir: 'dist',
+  generateBuildId: async () => 'build',
 };
 
 module.exports = nextConfig;
